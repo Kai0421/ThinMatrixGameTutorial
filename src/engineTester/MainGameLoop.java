d3069aeb21fdb1ff88204c76bff85e8b2da69c46
Ren Kai Tam <k_tam@hotmail.co.uk>
2017-12-28 01:27:29 +0000

    Episode 6 : Texturing

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,12 +1,14 @@
 package engineTester;
 
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -28,17 +30,25 @@ public class MainGameLoop {
 				0, 1, 3,	//Top Left Triangle (V0, V1, V3)
 				3, 1, 2		//Bottom right Triangle (V3, V1, V2)
 		};
+
+		float[] textureCoords= {
+				0, 0,	//V0
+				0, 1, 	//V1
+				1, 1, 	//V3
+				1, 0, 	//V2
+		};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()){
 			//gameLogic
 
-			
 			//render
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
